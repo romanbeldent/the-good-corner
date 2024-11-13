860262bf03df9b943bd004495bbe5176c2239261
RomanBeldent <romanbeldent@gmail.com>
2024-11-13 16:01:37 +0100

    feat: ajoute la création d'une nouvelle annonce

--- backend/src/resolvers/AdResolver.ts
+++ backend/src/resolvers/AdResolver.ts
@@ -1,5 +1,5 @@
 import { Ad } from "../entities/Ad";
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver(Ad)
 export class AdResolver {
@@ -12,4 +12,35 @@ export class AdResolver {
         })
         return ads;
     }
+
+    @Query(() => Ad)
+    async getAdById(
+        @Arg("id") id: number) {
+        const ad = await Ad.findOneBy({ id: id })
+        return ad;
+    }
+
+    @Mutation(() => Ad)
+    async createNewAd(
+        @Arg("title") title: string,
+        @Arg("description") description: string,
+        @Arg("owner") owner: string,
+        @Arg("price") price: number,
+        @Arg("picture") picture: string,
+        @Arg("location") location: string,
+    ) {
+        const ad = new Ad();
+        ad.title = title;
+        ad.description = description;
+        ad.owner = owner;
+        ad.price = price;
+        ad.picture = picture;
+        ad.location = location;
+        ad.createdAt = new Date();
+
+        const result = await ad.save();
+        return result;
+    }
 }
+
+export default AdResolver;
